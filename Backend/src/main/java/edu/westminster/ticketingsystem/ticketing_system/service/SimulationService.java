1767e85e0361fa88a352a87d596b27aba99790c5
Sandeep-Abeykoon <97498102+Sandeep-Abeykoon@users.noreply.github.com>
2024-12-07 13:07:04 +0530

    Update SimulationService.java

--- Backend/src/main/java/edu/westminster/ticketingsystem/ticketing_system/service/SimulationService.java
+++ Backend/src/main/java/edu/westminster/ticketingsystem/ticketing_system/service/SimulationService.java
@@ -1,6 +1,7 @@
 package edu.westminster.ticketingsystem.ticketing_system.service;
 
 import edu.westminster.ticketingsystem.ticketing_system.component.TicketPool;
+import edu.westminster.ticketingsystem.ticketing_system.model.Customer;
 import edu.westminster.ticketingsystem.ticketing_system.model.Vendor;
 import org.springframework.stereotype.Service;
 
@@ -10,20 +11,24 @@ import java.util.List;
 @Service
 public class SimulationService {
     private final ParticipantFactory participantFactory;
-    private final List<Thread> vendorThreads;
     private final TicketPool ticketPool;
+    private final List<Thread> vendorThreads;
+    private final List<Thread> customerThreads;
+
 
     public SimulationService(ParticipantFactory participantFactory, TicketPool ticketPool){
         this.participantFactory = participantFactory;
         this.ticketPool = ticketPool;
         this.vendorThreads = new ArrayList<>();
+        this.customerThreads = new ArrayList<>();
 
     }
-    public void startSimulation(int numberOfVendors) {
+    public void startSimulation(int numberOfVendors, int numberOfCustomers) {
         if (!vendorThreads.isEmpty()) {
             throw new IllegalStateException(("Simulation is already running"));
         }
 
+        // Starting vendor threads
         for (int i = 0; i < numberOfVendors; i++) {
             String vendorId = String.valueOf(i + 1);
 
@@ -32,6 +37,16 @@ public class SimulationService {
             vendorThreads.add(vendorThread);
             vendorThread.start();
         }
+
+        // Starting customer threads
+        for(int i = 0; i < numberOfCustomers; i++ ) {
+            String customerId = String.valueOf(i + 1);
+
+            Customer customer = participantFactory.createCustomer(customerId);
+            Thread customerThread = new Thread(customer);
+            customerThreads.add(customerThread);
+            customerThread.start();
+        }
     }
 
     public void stopSimulation() {
@@ -39,6 +54,11 @@ public class SimulationService {
             thread.interrupt();
         }
         vendorThreads.clear();
+
+        for (Thread thread : customerThreads) {
+            thread.interrupt();
+        }
+        customerThreads.clear();
 
         ticketPool.clearPool();
     }
