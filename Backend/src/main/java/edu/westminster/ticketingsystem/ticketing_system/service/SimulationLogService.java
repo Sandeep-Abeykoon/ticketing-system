462f747c96ebdf71d46d36a2963ab894a9fc2937
Sandeep-Abeykoon <97498102+Sandeep-Abeykoon@users.noreply.github.com>
2024-12-07 19:31:58 +0530

    Create SimulationLogService.java

---
+++ Backend/src/main/java/edu/westminster/ticketingsystem/ticketing_system/service/SimulationLogService.java
@@ -0,0 +1,23 @@
+package edu.westminster.ticketingsystem.ticketing_system.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class SimulationLogService {
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public void sendLog(String logMessage) {
+        messagingTemplate.convertAndSend("/topic/simulation-logs", logMessage);
+    }
+
+    public void sendTicketAvailability(int availableTickets) {
+        messagingTemplate.convertAndSend("/topic/ticket-availability", availableTickets);
+    }
+
+    public void sendSystemStatus(boolean isRunning) {
+        messagingTemplate.convertAndSend("/topic/system-status", isRunning);
+    }
+}
